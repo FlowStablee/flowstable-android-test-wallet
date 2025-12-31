c6d9a15c0d10b22b5a69e755dbec9010e3eddb0e
Pranav <github@pranav0x.xyz>
2026-01-01 01:34:40 +0530

    Update NetworkRepository: Use stable RPCs and change chainId to Long

--- app/src/main/java/com/antigravity/cryptowallet/data/blockchain/NetworkRepository.kt
+++ app/src/main/java/com/antigravity/cryptowallet/data/blockchain/NetworkRepository.kt
@@ -8,7 +8,7 @@ data class Network(
     val id: String,
     val name: String,
     val rpcUrl: String,
-    val chainId: Int,
+    val chainId: Long,
     val symbol: String,
     val coingeckoId: String,
     val explorerUrl: String? = null,
@@ -20,12 +20,12 @@ class NetworkRepository @Inject constructor() {
     private var _activeNetworkId = "eth"
     
     val networks = listOf(
-        Network("eth", "Ethereum", "https://eth.llamarpc.com", 1, "ETH", "ethereum", "https://etherscan.io", "https://assets.coingecko.com/coins/images/279/small/ethereum.png"),
-        Network("bsc", "BNB Chain", "https://binance.llamarpc.com", 56, "BNB", "binancecoin", "https://bscscan.com", "https://assets.coingecko.com/coins/images/825/small/bnb-icon2_2x.png"),
-        Network("matic", "Polygon", "https://polygon.llamarpc.com", 137, "POL", "matic-network", "https://polygonscan.com", "https://assets.coingecko.com/coins/images/4713/small/matic-token-icon.png"),
-        Network("base", "Base", "https://base.llamarpc.com", 8453, "ETH", "ethereum", "https://basescan.org", "https://assets.coingecko.com/coins/images/30983/small/base.png"),
-        Network("arb", "Arbitrum", "https://arbitrum.llamarpc.com", 42161, "ETH", "ethereum", "https://arbiscan.io", "https://assets.coingecko.com/coins/images/16547/small/photo_2023-03-29_21.47.00.jpeg"),
-        Network("op", "Optimism", "https://optimism.llamarpc.com", 10, "ETH", "ethereum", "https://optimistic.etherscan.io", "https://assets.coingecko.com/coins/images/25244/small/Optimism.png")
+        Network("eth", "Ethereum", "https://rpc.ankr.com/eth", 1L, "ETH", "ethereum", "https://etherscan.io", "https://assets.coingecko.com/coins/images/279/small/ethereum.png"),
+        Network("bsc", "BNB Chain", "https://bsc-dataseed.binance.org", 56L, "BNB", "binancecoin", "https://bscscan.com", "https://assets.coingecko.com/coins/images/825/small/bnb-icon2_2x.png"),
+        Network("matic", "Polygon", "https://polygon-rpc.com", 137L, "POL", "matic-network", "https://polygonscan.com", "https://assets.coingecko.com/coins/images/4713/small/matic-token-icon.png"),
+        Network("base", "Base", "https://rpc.ankr.com/base", 8453L, "ETH", "ethereum", "https://basescan.org", "https://assets.coingecko.com/coins/images/30983/small/base.png"),
+        Network("arb", "Arbitrum One", "https://arb1.arbitrum.io/rpc", 42161L, "ETH", "ethereum", "https://arbiscan.io", "https://assets.coingecko.com/coins/images/16547/small/photo_2023-03-29_21.47.00.jpeg"),
+        Network("op", "Optimism", "https://mainnet.optimism.io", 10L, "ETH", "ethereum", "https://optimistic.etherscan.io", "https://assets.coingecko.com/coins/images/25244/small/Optimism.png")
     )
     
     val activeNetwork: Network
